f396ceab24900f8fe144ba96821af0e571b0bb25
unknown <tpmalakkad@gmail.com>
2020-09-12 23:55:22 +0530

    Updates

---
+++ precache-manifest.a33f628fac0992ce267a671520abe980.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82c77f8c44ff7dd703155efdfac0b91",
+    "url": "/facereg/index.html"
+  },
+  {
+    "revision": "b4856c1928c0a374043a",
+    "url": "/facereg/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "735dc39f1fef239a4f2c",
+    "url": "/facereg/static/css/main.5d64566f.chunk.css"
+  },
+  {
+    "revision": "b4856c1928c0a374043a",
+    "url": "/facereg/static/js/2.d2fd8c9d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facereg/static/js/2.d2fd8c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735dc39f1fef239a4f2c",
+    "url": "/facereg/static/js/main.909b2868.chunk.js"
+  },
+  {
+    "revision": "3aac81ef3fb2bc555da6",
+    "url": "/facereg/static/js/runtime-main.08cdaabe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facereg/static/media/logo.5d5d9eef.svg"
+  }
+]);
